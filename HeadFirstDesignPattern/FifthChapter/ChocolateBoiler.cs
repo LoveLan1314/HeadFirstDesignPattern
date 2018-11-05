f198450d28bae9e735dcab133a7ba5ca7349127c
LoveLan1314 <736265181@qq.com>
2018-11-05 22:36:22 +0800

    单件模式

---
+++ HeadFirstDesignPattern/FifthChapter/ChocolateBoiler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeadFirstDesignPattern.FifthChapter
+{
+    public class ChocolateBoiler
+    {
+        private static ChocolateBoiler _uniqueInstance;
+        private bool _empty;
+        private bool _boiled;
+
+        private ChocolateBoiler()
+        {
+            _empty = true;
+            _boiled = false;
+        }
+
+        //多线程下存在问题
+        public static ChocolateBoiler GetInstance()
+        {
+            if(_uniqueInstance == null)
+            {
+                _uniqueInstance = new ChocolateBoiler();
+            }
+            return _uniqueInstance;
+        }
+
+        public void Fill()
+        {
+            if (IsEmpty())
+            {
+                _empty = false;
+                _boiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            if(!IsEmpty() && IsBoiled())
+            {
+                _empty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if(!IsEmpty() && !IsBoiled())
+            {
+                _boiled = true;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return _empty;
+        }
+
+        public bool IsBoiled()
+        {
+            return _boiled;
+        }
+    }
+}
---
+++ HeadFirstDesignPattern/FifthChapter/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeadFirstDesignPattern.FifthChapter
+{
+    public class Singleton
+    {
+        private static Singleton _uniqueInstance;
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetSingleton()
+        {
+            if(_uniqueInstance == null)
+            {
+                _uniqueInstance = new Singleton();
+            }
+            return _uniqueInstance;
+        }
+    }
+}
